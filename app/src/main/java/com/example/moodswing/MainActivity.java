ce6133968e07048ffa271471162493c82876a554
Gengyuan Huang <gengyuan@ualberta.ca>
2019-10-26 14:06:07 -0600

    implement signup functionality

--- app/src/main/java/com/example/moodswing/MainActivity.java
+++ app/src/main/java/com/example/moodswing/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         Intent intentLoginActivity = new Intent(this, LoginActivity.class);
+
         startActivity(intentLoginActivity);
 
     }
--- app/src/main/java/com/example/moodswing/RegisterActivity.java
+++ app/src/main/java/com/example/moodswing/RegisterActivity.java
@@ -1,16 +1,35 @@
 package com.example.moodswing;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     //
+    private static final String TAG = "RegisterActivity";
     TextView toLogin;
+    Button registerButton;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    FirebaseFirestore db;
+
 
 
     @Override
@@ -20,9 +39,10 @@ public class RegisterActivity extends AppCompatActivity {
 
         // link all elements
         toLogin = findViewById(R.id.switchToLogin);
-
-
-
+        registerButton = findViewById(R.id.registerButton);
+        usernameEditText = findViewById(R.id.regUser);
+        passwordEditText = findViewById(R.id.regPassword);
+        db = FirebaseFirestore.getInstance();
 
         // set listeners
         toLogin.setOnClickListener(new View.OnClickListener() {
@@ -31,47 +51,44 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-
-    }
-}
-
-    /*
-    public void createUser(){
-        final CollectionReference conllectionReference = db.collection("Accounts");
-        Button signup_Button = findViewById(R.id.signupButton);
-        Button register_Button = findViewById(R.id.registerButton);
-        EditText addUsernameEditText = findViewById(R.id.userField);
-        EditText addPasswordEditText = findViewById(R.id.passField);
-
-
-        signup_Button.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                final String userName = addUsernameEditText.getText().toString();
-                final String passWord = addPasswordEditText.getText,getText().toString();
-                HashMap<String, String> data = new HashMap<>();
-                if (userName.length() > 4 && passWord.length() > 4) {
-                    data.put("username", userName);
-                    collectionReference
-                            .document(userName)
-                            .set(data)
-                            .addOnSuccessListener(new OnSuccessListener<Void> () {
-                                @Override
-                                public void onSucess(Void aVoid) {
-                                    Log.d(TAG, "Username has successfully been added");
-                                }
-                            })
-                            .addOnFailureListened(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.d(TAG, "Password has successfully been added");
-                                }
-                            });
-                    signup_Button.setText("");
-                    addPasswordEditText.setText("");
-                }
+            public void onClick(View v) {
+                registerNewUser();
             }
         });
 
-    }*/
+
+    }
+
+    private void registerNewUser(){
+        // error handling missing here, for now - gengyuan
+        final CollectionReference accountsPath = db.collection("Accounts");
+        String userName = usernameEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        // error checking should be here
+        //
+        //
+
+        // key value pair - "username, password"
+        Map<String, String> newUser = new HashMap<>();
+        newUser.put(userName, password);
+
+        // add to database
+        accountsPath
+                .add(newUser)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "User creation successful with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Error adding user", e);
+                    }
+                });
+    }
+}
