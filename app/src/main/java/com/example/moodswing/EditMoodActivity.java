53671e6caab00617ce9d48372438c5e237c10517
gengyuanhuang <gengyuan@ualberta.ca>
2019-10-30 07:59:49 -0600

    roolback master to 50b7973
    rollback master to commit 50b7973 before alex's merge.

--- app/src/main/java/com/example/moodswing/EditMoodActivity.java
+++ app/src/main/java/com/example/moodswing/EditMoodActivity.java
@@ -2,13 +2,9 @@ package com.example.moodswing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -16,13 +12,12 @@ import android.widget.TextView;
 import com.google.protobuf.Empty;
 
 import java.io.Serializable;
-import java.util.Calendar;
 
 public class EditMoodActivity extends AppCompatActivity implements Serializable{
 
     EditText minuteText;
     EditText hourText;
-    TextView dateView;
+    EditText reasonEditText;
     ImageButton happyButton;
     ImageButton angryButton;
     ImageButton sadButton;
@@ -33,7 +28,9 @@ public class EditMoodActivity extends AppCompatActivity implements Serializable{
     private int moodType;
     private DateJar date;
     private TimeJar time;
-    private DatePickerDialog.OnDateSetListener dateListener;
+
+    private String reason;
+    private Integer socialSituation;
 
     FirestoreUserDocCommunicator communicator;
     String username;
@@ -52,81 +49,41 @@ public class EditMoodActivity extends AppCompatActivity implements Serializable{
 
         hourText = findViewById(R.id.Hours);
         minuteText = findViewById(R.id.Minutes);
+        //reasonEditText = findViewById(R.id.reasonEditText);
         happyButton = findViewById(R.id.happy_button);
         sadButton = findViewById(R.id.sad_button);
         angryButton = findViewById(R.id.angry_button);
         emotionalButton = findViewById(R.id.emotional_button);
         confirmButton = findViewById(R.id.confirmNewMood);
-        dateView = findViewById(R.id.dateView);
 
 
-        dateView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Calendar calendar = Calendar.getInstance();
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-                int month = calendar.get(Calendar.MONTH);
-                int year = calendar.get(Calendar.YEAR);
-
-                DatePickerDialog datepick = new DatePickerDialog( EditMoodActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateListener,year,month,day);
-                datepick.show();
-            }
-        });
-
-        dateListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                date = new DateJar(year,month,day);
-                Log.v("SOMETHING", year + "");
-                dateView.setText(Integer.toString(year));
-
-            }
-        };
 
         confirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                /*if (!dateText.getText().toString().equals("")){
+                    String[] datetemp = dateText.getText().toString().split("-");
+                    DateJar dateJar = new DateJar(Integer.parseInt(datetemp[0]),Integer.parseInt(datetemp[1]),Integer.parseInt(datetemp[2]));
+                    moodEvent.setDate(dateJar);}*/
                 String minutes = minuteText.getText().toString();
                 String hours = hourText.getText().toString();
                 if (!minutes.equals("") && !hours.equals("")){
+
                     TimeJar timeJar = new TimeJar(Integer.parseInt(minutes),Integer.parseInt(hours));
                     moodEvent.setTime(timeJar);}
-                if (date != null)
-                    moodEvent.setDate(date);
+                //reason = reasonEditText.getText().toString();
+                //moodEvent.setReason(reason);
                 communicator.editMood(moodEvent);
-                Intent backIntent = new Intent();
+                Intent backIntent = new Intent(EditMoodActivity.this,MoodDetailActivity.class);
                 backIntent.putExtra("UserName", username);
                 backIntent.putExtra("MoodEvent",  moodEvent);
-                setResult(Activity.RESULT_OK,backIntent);
-                finish();
+                startActivity(backIntent);
             }
         });
-
         happyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moodType = 1;
-            }
-        });
-        angryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moodType = 2;
-            }
-        });
-
-        emotionalButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moodType = 3;
-            }
-        });
-
-        sadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moodType = 4;
+                moodType = 0;
             }
         });
     }
--- app/src/main/java/com/example/moodswing/MainActivity.java
+++ app/src/main/java/com/example/moodswing/MainActivity.java
@@ -20,36 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private static final int REG_ACTIVITY_REQUEST_CODE = 2;
 
 
-    // RecyclerView related
-    private RecyclerView.LayoutManager recyclerViewLayoutManager;
-    private MoodAdapter moodListAdapter;
-    private ArrayList<MoodEvent> moodDataList;
-            // note: can add an array here for item deletion.
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
-
-
-        moodList.setAdapter(moodListAdapter);
-        moodList.setLayoutManager(recyclerViewLayoutManager);
-
-
-
-
-        /* ----------------------- IMPORTANT ---------------------*/
-        /* life cycle reminder
-         * this is the end of onCreate method
-         *
-         *
-         * for action that need to be performed after user loggin should be written in "onPostLogin()" method
-         * for action that need to be performed before user loggin should be written in "onCreate()" method BEFORE this msg
-         *
-         *
-         */
 
         /* login */
         toLogin();
@@ -100,61 +75,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-        if (requestCode == 9) {
-            if (resultCode == RESULT_OK) {
-                String UID = data.getStringExtra("UID");
-                if (UID != null)
-                    communicator.removeMoodEvent(UID);
-            }
-        }
-
     }
 
-
-    private void onPostLogin(String username){
-        /* --------------- init communicator (this should be on top)----------- */
-        communicator = new FirestoreUserDocCommunicator(username);
-        communicator.initMoodEventsList(moodList); // init listView realtimeListener by communicator
-
-        /* --------------------------- OnclickListeners ------------------------ */
-        // adding
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent i = new Intent(getApplicationContext(), NewMoodActivity.class);
-                startActivityForResult(i, 2);
-                // testing
-                //communicator.addMoodEvent(new MoodEvent(1, new DateJar(1998,2,27), new TimeJar(12,30)));
-                // can call some method here to switch activity.
-            }
-        });
-        // deletion
-        delButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                for(int i = 0; i < moodListAdapter.getItemCount(); i++){
-                    if (moodDataList.get(i).isSelected()) {
-                        communicator.removeMoodEvent(moodDataList.get(i).getUniqueID());
-                    }
-                }
-            }
-        });
-
-
-        /* ---------------------------- Other Actions ----------------------- */
-
-
-        moodListAdapter.setOnItemClickListener(new MoodAdapter.OnItemClickListener() {
-            @Override
-            public void OnItemiClick(int position) {
-                MoodEvent moodEvent = moodDataList.get(position);
-                Intent intent = new Intent(MainActivity.this,MoodDetailActivity.class);
-                intent.putExtra("MoodEvent",moodEvent);
-                startActivityForResult(intent,9);
-            }
-        });
-        //
-
-    }
 }
