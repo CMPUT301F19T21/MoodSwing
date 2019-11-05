d6caa77dee1e03bd0ddc39e1ae9d4858b3ff24d4
yinsongtang <yinsong1@ualberta.ca>
2019-11-05 14:50:06 -0700

    Update MainactivityTest.java

--- app/src/androidTest/java/com/example/moodswing/MainactivityTest.java
+++ app/src/androidTest/java/com/example/moodswing/MainactivityTest.java
@@ -6,21 +6,27 @@ import android.view.View;
 import android.widget.EditText;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.moodswing.Fragments.HomeFragment;
 import com.example.moodswing.customDataTypes.FirestoreUserDocCommunicator;
 import com.google.firebase.auth.FirebaseAuth;
-import com.robotium.solo.Solo;
+
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
-
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class MainactivityTest {
-    Solo solo;
     private FirebaseAuth mAuth;
     @Rule
     public IntentsTestRule<MainActivity> rule =
@@ -28,23 +34,23 @@ public class MainactivityTest {
     @Before
     public void setUp() throws Exception{
         mAuth = FirebaseAuth.getInstance();
-        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
         if (mAuth.getCurrentUser()==null){
             mAuth.signInWithEmailAndPassword("test@test.com", "123456");}
-
-        //   solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
-        //   solo.enterText((EditText) solo.getView(R.id.userEmailField), "test@test.com");
-        //   solo.enterText((EditText) solo.getView(R.id.passField), "123456");
-        //  View view = solo.getCurrentActivity().findViewById(R.id.loginComfirmBtn);
-        //  solo.clickOnView(view);
-
     }
 
     @Test
     public void CheckAddMood(){
-        solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.home_add));
-        solo.assertCurrentActivity("Not in AddMoodActivity", MainActivity.class);
-        
+        onView(withId(R.id.home_add))
+                .perform(click());
+        intended(hasComponent(NewMoodActivity.class.getName()));
+
+       // solo.assertCurrentActivity("Not in AddMoodActivity", MainActivity.class);
+
+    }
+
+    @Test
+    public void CheckEditMood(){
+       // solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.));
     }
 
 }
