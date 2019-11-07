cf1db246e3ebda05c0b26b756bdb1c3df592e6e5
Vinny51 <vinny.baijoo@gmail.com>
2019-11-06 23:27:51 -0800

    check if user is signed in before loginTest and sign out

--- app/src/androidTest/java/com/example/moodswing/LoginTest.java
+++ app/src/androidTest/java/com/example/moodswing/LoginTest.java
@@ -1,14 +1,17 @@
 package com.example.moodswing;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.example.moodswing.customDataTypes.FirestoreUserDocCommunicator;
 import com.google.api.Authentication;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -23,11 +26,13 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+//User must be logged out for this test to pass due to onStart
 public class LoginTest {
 
     private String loginEmail;
     private String password;
     private FirestoreUserDocCommunicator communicator;
+    private FirebaseAuth mAuth;
 
     @Rule
     public IntentsTestRule<LoginActivity> intentsTestRule =
@@ -37,16 +42,16 @@ public class LoginTest {
     public void initCredentials() {
         loginEmail = "test@mail.com";
         password = "123456";
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser()!=null) {
+            Log.d("test", "user exists");
+            FirestoreUserDocCommunicator.destroy();
+        }
 
 
     }
 
-
-
-    @Test
-    public void validateIntentSentToPackage() {
-
-    }
+    
 
 
     @Test
