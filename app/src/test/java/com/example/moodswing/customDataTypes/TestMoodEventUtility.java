8849bf0e6b7f61a5a1b94c025f387c50459324d6
Vinny51 <vinny.baijoo@gmail.com>
2019-11-06 20:15:06 -0800

    unit test cases

--- app/src/test/java/com/example/moodswing/customDataTypes/TestMoodEventUtility.java
+++ app/src/test/java/com/example/moodswing/customDataTypes/TestMoodEventUtility.java
@@ -2,11 +2,18 @@ package com.example.moodswing.customDataTypes;
 
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestMoodEventUtility {
 
     @Test
     public void testReturnMonthStr() {
         MoodEvent mood = new MoodEvent();
+        DateJar date = new DateJar(1993,10,10);
+        mood.setDate(date);
 
+        assertEquals(mood.getDate(), date);
     }
+
+
 }
