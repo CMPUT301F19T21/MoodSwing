d7ec22053b7e72c240ea832be6d1470fc8ddb5cd
Vinny51 <vinny.baijoo@gmail.com>
2019-11-25 18:15:00 -0800

    followingTest

--- app/src/androidTest/java/com/example/moodswing/MainactivityTest.java
+++ app/src/androidTest/java/com/example/moodswing/MainactivityTest.java
@@ -268,23 +268,30 @@ public class MainactivityTest {
      * Test follow function is work
      */
     //Test Following not complete since some function for following is not complete such as delete following
+    //For this test to work there must be a user with username 'usera'
     @Test
     public void CheckFollowing(){
         onView(withId(R.id.nav_followingBtn))
                 .perform(click());
         //check if in the following screen shows
-        onView(withId(R.id.following_list)).check(matches(isDisplayed()));//check if in the FollowingFragment
-        // check following manager
-        onView(withId(R.id.following_management))
-                .perform(click());
+        try {
+            onView(withId(R.id.following_list)).check(matches(isDisplayed()));//check if in the FollowingFragment
+            onView(withId(R.id.following_management)).perform(click());
+        }
+        catch(NoMatchingViewException e) {
+            onView(withText("NO ONE IS SHARING THEIR MOOD CLICK TO FOLLOW PEOPLE")).check(matches(isDisplayed()));
+            onView(withId(R.id.emptyNotifcation_Btn)).perform(click());
+        }
+
         onView(withId(R.id.managment_following)).check(matches(isDisplayed()));//check if in the FollowingFragment
-        // check follow someone
         onView(withId(R.id.request_button))
                 .perform(click());
         onView(withId(R.id.requestFrag_requestEnter))
-                .perform(typeText("yinsong"),closeSoftKeyboard());
+                .perform(typeText("usera"),closeSoftKeyboard());
         onView(withId(R.id.requestFrag_confirm))
                 .perform(click());
+
+        
 
         // accept request
 /*
